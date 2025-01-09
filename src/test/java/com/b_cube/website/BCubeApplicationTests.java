6ff97a1da6bfcef5329e10f3cf06c28a45766721
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-01-09 21:35:55 +0900

    비큐브 웹사이트 환경 설정

---
+++ src/test/java/com/b_cube/website/BCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b_cube.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
