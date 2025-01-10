2ff69c2be46314164f6771f0eb557da1aef3cfee
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-01-10 14:39:11 +0900

    feat: 전역 예외 처리 구현

---
+++ src/main/java/com/b_cube/website/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.b_cube.website.global.exception;
+
+import com.b_cube.website.domain.executives.exception.ExecutivesNotFoundException;
+import com.b_cube.website.global.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ExecutivesNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleExecutivesNotFoundException(ExecutivesNotFoundException e) {
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .message(e.getMessage())
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
+
+}
