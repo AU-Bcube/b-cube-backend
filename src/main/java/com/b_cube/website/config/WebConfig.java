0a47e1b0824f338adcf27b1eef1fff755462a0fc
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-01-13 17:28:21 +0900

    fix: CORS 정책 수정

--- src/main/java/com/b_cube/website/config/WebConfig.java
+++ src/main/java/com/b_cube/website/config/WebConfig.java
@@ -1,14 +1,16 @@
 package com.b_cube.website.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
