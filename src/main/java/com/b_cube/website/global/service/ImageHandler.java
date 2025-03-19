a5c0f034dae4929c4ebd54006c077755cc426774
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-19 21:05:42 +0900

    chore: 정적 파일 경로 application.properties에 보관

--- src/main/java/com/b_cube/website/global/service/ImageHandler.java
+++ src/main/java/com/b_cube/website/global/service/ImageHandler.java
@@ -1,5 +1,6 @@
 package com.b_cube.website.global.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,6 +9,13 @@ import java.io.IOException;
 
 @Service
 public class ImageHandler {
+
+    @Value("${file.upload.img.dir}")
+    private String uploadImgDir;
+    @Value("${file.upload.pdf.dir}")
+    private String uploadPdfDir;
+    @Value("${file.access.dir}")
+    private String accessDir;
 
     /**
      * MultipartFile을 받아서 지정된 경로에 이미지를 저장하는 기능
@@ -18,11 +26,22 @@ public class ImageHandler {
     public String saveImage(MultipartFile image) throws IOException {
         // 정적 파일 저장
         String fileName = getOriginName(image);
-        String fullPathName = "/home/jinyoung/imgs" + fileName;
+        String fullPathName = uploadImgDir + fileName;
         image.transferTo(new File(fullPathName));
 
         // Nginx를 통해 접근 가능한 URL 생성
-        String fileUrl = "https://b-cube.kr/imgs" + fileName;
+        String fileUrl = accessDir + fileName;
+        return fileUrl;
+    }
+
+    public String savePDF(MultipartFile pdf) throws IOException {
+        // 정적 파일 저장
+        String fileName = getOriginName(pdf);
+        String fullPathName = uploadPdfDir + fileName;
+        pdf.transferTo(new File(fullPathName));
+
+        // Nginx를 통해 접근 가능한 URL 생성
+        String fileUrl = accessDir + fileName;
         return fileUrl;
     }
 
