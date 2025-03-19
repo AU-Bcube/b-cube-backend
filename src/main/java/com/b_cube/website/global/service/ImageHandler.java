5a11f50adff81682d5827f773a2a7d7114947c20
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-19 22:55:54 +0900

    chore: pdf와 img 경로 분리

--- src/main/java/com/b_cube/website/global/service/ImageHandler.java
+++ src/main/java/com/b_cube/website/global/service/ImageHandler.java
@@ -14,6 +14,10 @@ public class ImageHandler {
     private String accessImgDir;
     @Value("${file.access.pdf.dir}")
     private String accessPdfDir;
+    @Value("${file.upload.pdf.dir}")
+    private String uploadPdfDir;
+    @Value("${file.upload.img.dir}")
+    private String uploadImgDir;
 
     /**
      * MultipartFile을 받아서 지정된 경로에 이미지를 저장하는 기능
@@ -24,7 +28,7 @@ public class ImageHandler {
     public String saveImage(MultipartFile image) throws IOException {
         // 정적 파일 저장
         String fileName = getOriginName(image);
-        String fullPathName = "/home/jinyoung/imgs/" + fileName;
+        String fullPathName = uploadImgDir + fileName;
         image.transferTo(new File(fullPathName));
 
         // Nginx를 통해 접근 가능한 URL 생성
@@ -35,7 +39,7 @@ public class ImageHandler {
     public String savePDF(MultipartFile pdf) throws IOException {
         // 정적 파일 저장
         String fileName = getOriginName(pdf);
-        String fullPathName = "/home/jinyoung/pdf/" + fileName;
+        String fullPathName = uploadPdfDir + fileName;
         pdf.transferTo(new File(fullPathName));
 
         // Nginx를 통해 접근 가능한 URL 생성
