42465fb33795b46019b8f602fa45e2fa728a20da
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-19 20:53:06 +0900

    feat: 로컬에 정적 파일 저장하는 ImageHandler 구현

---
+++ src/main/java/com/b_cube/website/global/service/ImageHandler.java
@@ -0,0 +1,32 @@
+package com.b_cube.website.global.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class ImageHandler {
+
+    /**
+     * MultipartFile을 받아서 지정된 경로에 이미지를 저장하는 기능
+     * @param image
+     * @return 경로 반환
+     * @throws IOException
+     */
+    public String saveImage(MultipartFile image) throws IOException {
+        // 정적 파일 저장
+        String fileName = getOriginName(image);
+        String fullPathName = "/home/jinyoung/imgs" + fileName;
+        image.transferTo(new File(fullPathName));
+
+        // Nginx를 통해 접근 가능한 URL 생성
+        String fileUrl = "https://b-cube.kr/imgs" + fileName;
+        return fileUrl;
+    }
+
+    private String getOriginName(MultipartFile image) {
+        return image.getOriginalFilename();
+    }
+}
