d01725821449f37ca29466f6c8adb4a9c18c3447
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-19 20:52:44 +0900

    feat: IOException 전역 예외 처리 핸들러에 추가

--- src/main/java/com/b_cube/website/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/b_cube/website/global/exception/GlobalExceptionHandler.java
@@ -12,11 +12,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
         @ExceptionHandler({ExecutivesNotFoundException.class, DesigntonNotFoundException.class, SexyItNotFoundException.class, EtcNotFoundException.class, ContactNotFoundException.class, ContactAlreadyExistsException.class})
         public ResponseEntity<ErrorResponse> handleExecutivesNotFoundException(RuntimeException e) {
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .message(e.getMessage())
+                    .build(), HttpStatus.NOT_FOUND);
+        }
+
+        @ExceptionHandler(IOException.class)
+        public ResponseEntity<ErrorResponse> handleIOException(IOException e) {
             return new ResponseEntity<>(ErrorResponse.builder()
                     .message(e.getMessage())
                     .build(), HttpStatus.NOT_FOUND);
