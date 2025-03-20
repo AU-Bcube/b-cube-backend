669d317402e2b7d00c3d2f970da8da4dd93cb6d1
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-20 16:15:02 +0900

    feat: 전역 예외 처리 핸들러에 FileNotFoundException.Java, StudyNotFoundException.Java 추가

--- src/main/java/com/b_cube/website/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/b_cube/website/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.b_cube.website.domain.designton.exception.DesigntonNotFoundException;
 import com.b_cube.website.domain.etc.exception.EtcNotFoundException;
 import com.b_cube.website.domain.executives.exception.ExecutivesNotFoundException;
 import com.b_cube.website.domain.sexyit.exception.SexyItNotFoundException;
+import com.b_cube.website.domain.study.exception.StudyNotFoundException;
 import com.b_cube.website.global.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ import java.io.IOException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-        @ExceptionHandler({ExecutivesNotFoundException.class, DesigntonNotFoundException.class, SexyItNotFoundException.class, EtcNotFoundException.class, ContactNotFoundException.class, ContactAlreadyExistsException.class})
+        @ExceptionHandler({FileNotFoundException.class, StudyNotFoundException.class, ExecutivesNotFoundException.class, DesigntonNotFoundException.class, SexyItNotFoundException.class, EtcNotFoundException.class, ContactNotFoundException.class, ContactAlreadyExistsException.class})
         public ResponseEntity<ErrorResponse> handleExecutivesNotFoundException(RuntimeException e) {
             return new ResponseEntity<>(ErrorResponse.builder()
                     .message(e.getMessage())
