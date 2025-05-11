420eb5874d442f78fc56bb0e8670c93fd616eb9d
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-05-11 21:15:29 +0900

    feat: WebConfig 추가

---
+++ src/main/java/com/b_cube/website/config/WebConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 API 경로에 대해
+                        .allowedOrigins("http://localhost:3000") // 프론트 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용 메서드
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 쿠키 등 인증정보 허용
+            }
+        };
+    }
+}
