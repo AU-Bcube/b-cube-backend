cd5fd7332256e51c92a4f758a34e5560a17933af
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-05-11 21:28:01 +0900

    fix: WebConfig 수정

--- src/main/java/com/b_cube/website/config/WebConfig.java
+++ src/main/java/com/b_cube/website/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.b_cube.website.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
